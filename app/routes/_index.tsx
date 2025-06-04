6c2bd81ff2c5f0ea05ac74f59ad13e086724a5d3
ChaitanyaVardhan <chaitanyavardhan1@gmail.comm>
2025-06-04 20:55:47 +0530

    dropbox

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -22,7 +22,7 @@ export default function Index() {
         <ul className="list-disc list-inside space-y-1">
           <li><a href="/google" className="text-blue-600 underline">Google</a></li>
           <li><a href="/salesforce" className="text-blue-600 underline">Salesforce</a></li>
-          <li><a href="/facebook" className="text-blue-600 underline">Facebook</a></li>
+          <li><a href="/dropbox" className="text-blue-600 underline">Dropbox</a></li>
           <li><a href="/jira-software-cloud" className="text-blue-600 underline">Jira</a></li>
         </ul>
       </div>
