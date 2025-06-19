daf944dcbc33c9e87d42080ed4ca8229231c7465
ChaitanyaVardhan <chaitanyavardhan1@gmail.comm>
2025-06-19 09:33:11 +0530

    remove same site strict

--- app/utils/backend.cookie.ts
+++ app/utils/backend.cookie.ts
@@ -1,12 +1,10 @@
 import {createCookie, createCookieSessionStorage} from "@remix-run/node"
 import { randomBytes } from "crypto"
 
-const isProd = process.env.NODE_ENV === "production";
-
 export const curlmateKeyCookie = createCookie("Curlmate", {
      httpOnly: true,
-     secure: isProd,
-     sameSite: "strict",
+     secure: false,
+     sameSite: "lax",
      path: "/",
      maxAge: 60 * 60 * 24
 })
