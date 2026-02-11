2e659488c3d43a71d964a145d5dc19bbaef5d486
ChaitanyaVardhan <chaitanyavardhan1@gmail.comm>
2026-02-11 19:41:16 +0530

    wip

---
+++ app/routes/api.create-api-key.ts
@@ -0,0 +1,75 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { verifyJwt } from "~/utils/backend.jwt";
+import { getApp, getSessionUser } from "~/utils/backend.redis";
+import { getRefreshToken } from "~/utils/backend.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const authHeader = request.headers.get("Authorization");
+  const jwt = authHeader?.split(" ")[1];
+  if (!jwt) {
+    return Response.json(
+      {
+        error: "No jwt found in Authorization header",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  try {
+    const decodedValue = verifyJwt(jwt);
+    if (!decodedValue) {
+      return Response.json({
+        error: "JWT could not be decoded",
+      });
+    }
+    const { sub } = decodedValue;
+
+    const connection = request.headers.get("x-connection");
+    if (!connection) {
+      return Response.json("x-connection header missing");
+    }
+
+    const [appHash, tokenUuid, service] = connection.split(":");
+    const user = await getSessionUser(`user:${sub}`);
+
+    if (!user) {
+      return Response.json({
+        error: "User not found",
+      });
+    }
+    const { apps } = user;
+    const appKey = `app:${appHash}:${service}`;
+    const idx1 = apps?.indexOf(appKey);
+    if (idx1 === -1) {
+      return Response.json({
+        error: "app not found",
+      });
+    }
+
+    const app = await getApp({ appHash, service });
+    if (!app) {
+      return Response.json({
+        error: "App not found",
+      });
+    }
+    const tokenKey = `token:${tokenUuid}`;
+    const idx2 = app.tokens.indexOf(tokenKey);
+    if (idx2 === -1) {
+      return Response.json({
+        error: "Access token not found",
+      });
+    }
+
+    const res = await getRefreshToken({ appHash, tokenUuid, service });
+    return Response.json({
+      accessToken: res.accessToken,
+    });
+  } catch (error: unknown) {
+    const err = error as Error;
+    return Response.json({
+      error: err.message,
+    });
+  }
+};
--- app/utils/backend.api.ts
+++ app/utils/backend.api.ts
@@ -1,15 +1,18 @@
-export async function createApiKey(useStorage= true ): Promise<{
-  key : string
-  encryptedKey?: string    
+import { randomBytes } from "crypto";
+export async function createApiKey(useStorage = true): Promise<{
+  key: string;
+  encryptedKey?: string;
 }> {
   try {
     const hasEncryptionKey = process.env.API_ENCRYPTION_KEY !== undefined
 
-    const plainKey = hasEncryptionKey ? generateEncryptedApiKey() : genereateApiKey();
+    const plainKey = hasEncryptionKey
+      ? generateEncryptedApiKey()
+      : genereateApiKey();
 
-    if(useStorage) {
+    if (useStorage) {
       const encryptedKey = await encryptApiKeyForStorage(plainKey);
-      return { key: plainKey, encryptedKey};
+      return { key: plainKey, encryptedKey };
     }
 
     return { key: plainKey };
@@ -20,10 +23,30 @@ export async function createApiKey(useStorage= true ): Promise<{
 
 export async function encryptApiKeyForStorage(apiKey: string): Promise<string> {
   try {
-    const { encrypted } = await encryptApiKeyForStorage(apiKey);
+    const encrypted = await encryptApiKey(apiKey);
     return encrypted;
   } catch (error) {
     throw new Error("Failed to encrypt API key");
   }
 }
 
+export function generateEncryptedApiKey(): string {
+  return `sk-cm-${randomBytes(24).toString("base64url")}`;
+}
+
+export function genereateApiKey(): string {
+  return `cm-${randomBytes(24).toString("base64url")}`;
+}
+
+export async function encryptApiKey(apiKey: string) {
+  const key = getEncryptionKey();
+
+  //backward compatibility, no encryption key set
+  if (!key) {
+    return { encrypted: apiKey, iv: "" };
+  }
+
+  const iv = randomBytes(16);
+
+  
+}
