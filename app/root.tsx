c05544e8d75ba33d0145f4ca3102ed91b07f0cfd
Chaitanya Vardhan <chaitanyavardhan1@gmail.com>
2026-03-03 17:17:27 +0530

    Remove Google Analytics from root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -33,21 +33,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-HG84CLGPWJ"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-HG84CLGPWJ');
-            `,
-          }}
-        />
         {/* Iubenda Cookie Solution Config */}
         <script
           type="text/javascript"
